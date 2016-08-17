12b025a50171b2ae98aaa3b153f5beba7b45d3c5
motjuste <dada3mn@gmail.com>
2016-08-17 15:36:42 +0200

    Added Sanity Checks for important packages

---
+++ test/test_sanity/test_packages.py
@@ -0,0 +1,20 @@
+import pytest
+
+@pytest.mark.trivial
+@pytest.mark.sanity_check
+def test_numpy_installed():
+    import numpy
+    assert True
+
+@pytest.mark.trivial
+@pytest.mark.sanity_check
+def test_scipy_installed():
+    import scipy
+    assert True
+
+@pytest.mark.trivial
+@pytest.mark.sanity_check
+def test_tensorflow_installed():
+    import tensorflow
+    assert True
+
