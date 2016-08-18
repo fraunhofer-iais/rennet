9c61a0a02bec9f702998fb8f70ab51f1d66e50d5
motjuste <dada3mn@gmail.com>
2016-08-18 11:33:35 +0200

    Environment setup info
    - Added shell script to keep track of conda commands run over time
    - Added exports of the environment

---
+++ conda-env-setup-commands.sh
@@ -0,0 +1,22 @@
+echo "Starting with r3"
+conda create -n r3 python=3.5 numpy scipy
+source activate r3
+conda install -n r3 -c conda-forge numpy scipy tensorflow
+conda install -n r3 -c conda-forge pip
+conda install -n r3 -c conda-forge pytest
+conda install -n r3 jupyter
+conda install -n r3 -c conda-forge matplotlib
+conda install -n r3 -c conda-forge pylint
+pip install yapf
+pip install https://github.com/ipython-contrib/jupyter_contrib_nbextensions/tarball/master
+conda install click
+
+echo "Done with r3"
+echo "Starting with r2"
+conda create -n r2 python=2.7 numpy scipy
+source activate r2
+conda install -n r2 -c conda-forge numpy scipy tensorflow pip pytest matplotlib pylint
+conda install -n r2 jupyter
+pip install yapf
+pip install https://github.com/ipython-contrib/jupyter_contrib_nbextensions/tarball/master
+conda install -n r2 click
