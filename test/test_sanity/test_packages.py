9be454b13cdd630a07150f9042e5a5554f9977cb
motjuste <dada3mn@gmail.com>
2016-08-24 00:23:38 +0200

    Added test for ffmpeg available

--- test/test_sanity/test_packages.py
+++ test/test_sanity/test_packages.py
@@ -1,4 +1,6 @@
 import pytest
+from rennet.utils.audio_utils import is_ffmpeg_available
+
 
 @pytest.mark.trivial
 @pytest.mark.sanity_check
@@ -6,11 +8,13 @@ def test_numpy_installed():
     import numpy
     assert True
 
+
 @pytest.mark.trivial
 @pytest.mark.sanity_check
 def test_scipy_installed():
     import scipy
     assert True
+
 
 @pytest.mark.trivial
 @pytest.mark.sanity_check
@@ -18,8 +22,15 @@ def test_tensorflow_installed():
     import tensorflow
     assert True
 
+
 @pytest.mark.trivial
 @pytest.mark.sanity_check
 def test_pydub_installed():
     import pydub
     assert True
+
+
+@pytest.mark.trivial
+@pytest.mark.sanity_check
+def test_ffmpeg_available():
+    assert is_ffmpeg_available()
