6432f24005d8aad1ce6b2faf3907169baec1f69e
motjuste <dada3mn@gmail.com>
2016-08-26 12:18:16 +0200

    lable utils initial commit

---
+++ rennet/utils/label_utils.py
@@ -0,0 +1,6 @@
+"""
+@motjuste
+Created: 26-08-2016
+
+Utilities for working with labels    
+"""
---
+++ test/test_utils/test_label_utils.py
@@ -0,0 +1,8 @@
+"""
+@motjuste
+Created: 26-08-2016
+
+Test the label utilities module
+"""
+import pytest
+from rennet.utils import label_utils as lu
