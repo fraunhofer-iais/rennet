2bbcb5d7d896bd9d4764528cc421c6e2a9e7ba1d
motjuste <dada3mn@gmail.com>
2016-09-05 22:22:24 +0200

    Some modifications to the qsub scripts

--- qsub/check-tensorflow.py
+++ qsub/check-tensorflow.py
@@ -2,10 +2,22 @@ from __future__ import print_function
 import time
 
 print()
-print(time.asctime())
+print("/ {:/<80}".format(str(time.asctime()) + " "))
 
 import sys
 print("\nPython Version:", sys.version)
 
-import tensorflow as tg
-print("\nTesnorflow Version:", tg.__version__)
+try:
+    import tensorflow as tg
+    print("\nTesnorflow Version:", tg.__version__)
+except:
+    print("\nProblem Loading tensorflow")
+
+try:
+    import six
+    print("\nCan Load six")
+except:
+    print("\nProblem loading six")
+
+import rennet.utils.audio_utils as au
+print("\nCan Load rennet\n")
