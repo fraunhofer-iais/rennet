e2f5c8b3acaab8253be9e4819ddf2877ac5837a6
motjuste <dada3mn@gmail.com>
2017-04-20 17:31:34 +0200

    move qsub to scripts folder

--- qsub/20161024-gertvmodel0.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-7.5/lib64/:/opt/software/cudnn/cudnn_v4.0/lib64/
-
-export RENNET_ROOT=/home/aabdullah/delve/rennet
-
-python $RENNET_ROOT/rennet/models/20161024-gertv1k-0.py
-
--- qsub/common.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-7.5/lib64/:/opt/software/cudnn/cudnn_v4.0/lib64/
-
-PROJECT_ROOT=$PWD
-export PYTHONPATH=$PROJECT_ROOT:$PYTHONPATH
