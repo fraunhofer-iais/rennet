4a7a973030ee11d514455202e5fec5799f6ff94a
motjuste <dada3mn@gmail.com>
2017-07-06 19:52:09 +0200

    Formatting

--- rennet/utils/py_utils.py
+++ rennet/utils/py_utils.py
@@ -67,7 +67,8 @@ class BaseSlotsOnlyClass(object):  #pylint: disable=too-few-public-methods
     def __repr__(self):
         a_v = ((att, getattr(self, att)) for att in self.__slots__)
         r = ".".join((self.__module__.split(".")[-1], self.__class__.__name__))
-        return r + "({})".format(", ".join("{!s}={!r}".format(*av) for av in a_v))
+        return r + "({})".format(", ".join("{!s}={!r}".format(*av)
+                                           for av in a_v))
 
 
 def getsize(obj_0):
@@ -76,14 +77,15 @@ def getsize(obj_0):
     Reference
         https://stackoverflow.com/a/30316760
     """
-    try: # Python 2
+    try:  # Python 2
         zero_depth_bases = (basestring, Number, xrange, bytearray)
         iteritems = 'iteritems'
-    except NameError: # Python 3
+    except NameError:  # Python 3
         zero_depth_bases = (str, bytes, Number, range, bytearray)
         iteritems = 'items'
 
     _seen_ids = set()
+
     def inner(obj):
         obj_id = id(obj)
         if obj_id in _seen_ids:
@@ -91,17 +93,21 @@ def getsize(obj_0):
         _seen_ids.add(obj_id)
         size = getsizeof(obj)
         if isinstance(obj, zero_depth_bases):
-            pass # bypass remaining control flow and return
+            pass  # bypass remaining control flow and return
         elif isinstance(obj, (tuple, list, Set, deque)):
             size += sum(inner(i) for i in obj)
         elif isinstance(obj, Mapping) or hasattr(obj, iteritems):
-            size += sum(inner(k) + inner(v) for k, v in getattr(obj, iteritems)())
+            size += sum(
+                inner(k) + inner(v) for k, v in getattr(obj, iteritems)())
         # Check for custom object instances - may subclass above too
         if hasattr(obj, '__dict__'):
             size += inner(vars(obj))
-        if hasattr(obj, '__slots__'): # can have __slots__ with __dict__
-            size += sum(inner(getattr(obj, s)) for s in obj.__slots__ if hasattr(obj, s))
+        if hasattr(obj, '__slots__'):  # can have __slots__ with __dict__
+            size += sum(
+                inner(getattr(obj, s)) for s in obj.__slots__
+                if hasattr(obj, s))
         return size
+
     return inner(obj_0)
 
 
