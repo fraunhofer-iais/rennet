016e72fb1bdabe0644fc8a0f904397b04bbd3ef0
motjuste <dada3mn@gmail.com>
2017-07-06 17:51:40 +0200

    Python utility to get size of objects
    Check the reference:
    https://stackoverflow.com/a/30316760

--- rennet/utils/py_utils.py
+++ rennet/utils/py_utils.py
@@ -5,6 +5,9 @@ Created: 10-10-2016
 Pure Python utilities
 """
 import re
+from sys import getsizeof
+from numbers import Number
+from collections import Set, Mapping, deque
 
 
 def is_string(obj):
@@ -64,3 +67,38 @@ class BaseSlotsOnlyClass(object):  #pylint: disable=too-few-public-methods
         a_v = ((att, getattr(self, att)) for att in self.__slots__)
         r = ".".join((self.__module__.split(".")[-1], self.__class__.__name__))
         return r + "({})".format(", ".join("{!s}={!r}".format(*av) for av in a_v))
+
+
+def getsize(obj_0):
+    """Recursively iterate to sum size of object & members.
+
+    Reference
+        https://stackoverflow.com/a/30316760
+    """
+    try: # Python 2
+        zero_depth_bases = (basestring, Number, xrange, bytearray)
+        iteritems = 'iteritems'
+    except NameError: # Python 3
+        zero_depth_bases = (str, bytes, Number, range, bytearray)
+        iteritems = 'items'
+
+    _seen_ids = set()
+    def inner(obj):
+        obj_id = id(obj)
+        if obj_id in _seen_ids:
+            return 0
+        _seen_ids.add(obj_id)
+        size = getsizeof(obj)
+        if isinstance(obj, zero_depth_bases):
+            pass # bypass remaining control flow and return
+        elif isinstance(obj, (tuple, list, Set, deque)):
+            size += sum(inner(i) for i in obj)
+        elif isinstance(obj, Mapping) or hasattr(obj, iteritems):
+            size += sum(inner(k) + inner(v) for k, v in getattr(obj, iteritems)())
+        # Check for custom object instances - may subclass above too
+        if hasattr(obj, '__dict__'):
+            size += inner(vars(obj))
+        if hasattr(obj, '__slots__'): # can have __slots__ with __dict__
+            size += sum(inner(getattr(obj, s)) for s in obj.__slots__ if hasattr(obj, s))
+        return size
+    return inner(obj_0)
