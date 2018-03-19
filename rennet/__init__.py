c80cb4f19b853b6f6942ef0b8bd05091466f368b
motjuste <dada3mn@gmail.com>
2018-03-19 16:03:32 +0100

    ~ Single sourcing version string
    Based on the *third* recommendation at:
    https://packaging.python.org/guides/single-sourcing-package-version/#single-sourcing-the-version
    
    There should be one-- and preferably only one --obvious way to do this.
    This is isn't.

--- rennet/__init__.py
+++ rennet/__init__.py
@@ -1,4 +1,6 @@
-__version__ = "0.2.0"
+from __future__ import absolute_import
+
+from .version import VERSION as __version__
 
 # IDEA: Implement keras like `get` functions to identify functions and classes by strings,
 # and 'serializing' and 'desirializing' them to and from h5 model files.
---
+++ rennet/version.py
@@ -0,0 +1,5 @@
+"""rennet version information
+@motjuste
+Created: 19-03-2018
+"""
+VERSION = "0.2.0"
