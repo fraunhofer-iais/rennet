ea21cb88af8ef5f35b11ae83eb8ac56a6bfffa1c
motjuste <dada3mn@gmail.com>
2018-03-31 13:16:32 +0200

    * Fix needing to call lctxfn regardless of lctx value
    - This was too darn complicated, and all those fixes were stupid
    - Needs proper refactoring

--- rennet/utils/h5_utils.py
+++ rennet/utils/h5_utils.py
@@ -228,9 +228,10 @@ class BaseWithContextPrepper(BaseH5ChunkPrepper):  # pylint: disable=abstract-me
                 label = label[:, self.dctx:self.dctx + 1, ...]
             else:
                 label = label[:, self.dctx - self.lctx:self.dctx + self.lctx + 1, ...]
-                label = self.lctxfn(label)
         else:
             label = label[:, np.newaxis, ...]
+            
+        label = self.lctxfn(label)
 
         return (data[..., None], label) if self.add_channel else (data, label)
 
